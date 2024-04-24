144df274ec808bf3e4274fd8832e6ee2c9800786
Gustavo-Blog <info@villcode.com>
2024-04-24 10:19:49 -0600

    Mi catorceavo commit

--- bustling-bar/src/consts.ts
+++ bustling-bar/src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
-export const USER_NICKNAME = 'EveSunMaple';
-export const USER_DESCRIPTION = 'a student';
+export const SITE_TITLE = 'Edgar Villegas - Compartiendo la vida del diseño y la tecnología';
+export const SITE_DESCRIPTION = 'Bienvenido a mi sitio web!';
+export const USER_NICKNAME = 'Aomine';
+export const USER_DESCRIPTION = 'un estudiante';
 export const TRANSITION_API = true;
