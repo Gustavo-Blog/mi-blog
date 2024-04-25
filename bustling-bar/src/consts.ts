adb405d0dea87b9e6194f78858ed520712f8e584
Gustavo-Blog <info@villcode.com>
2024-04-24 18:52:43 -0600

    Mi veintitresavo commit

--- bustling-bar/src/consts.ts
+++ bustling-bar/src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Edgar Villegas - Compartiendo mi pasión por el diseño y la tecnología';
-export const SITE_DESCRIPTION = 'Bienvenido a mi sitio web!';
+export const SITE_TITLE = 'A Programmer\'s Mind ❤️ - Blog';
+export const SITE_DESCRIPTION = 'Compartiendo mi pasión por el desarrollo web';
 export const USER_NICKNAME = 'Aomine';
 export const USER_DESCRIPTION = 'un estudiante';
 export const TRANSITION_API = true;
